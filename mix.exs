c4e1b89644606bfde6bd3d0ebed39a633d96e26c
Susumu Yamazaki <zacky1972@gmail.com>
2020-11-29 01:00:30 +0900

    set automake

--- mix.exs
+++ mix.exs
@@ -9,7 +9,8 @@ defmodule SwiftElixirTest.MixProject do
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       compilers: [:elixir_make] ++ Mix.compilers,
-      aliases: [compile: [&configure/1]]
+      aliases: [compile: [&configure/1, "compile"]],
+      make_clean: ["clean"]
     ]
   end
 
