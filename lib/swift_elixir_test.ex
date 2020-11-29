f70a03db1fa9db1026573cf211d36066051ef43e
Susumu Yamazaki <zacky1972@gmail.com>
2020-11-29 12:51:22 +0900

    implement NIF code, temporally

--- lib/swift_elixir_test.ex
+++ lib/swift_elixir_test.ex
@@ -1,5 +1,21 @@
 defmodule SwiftElixirTest do
+  require Logger
+
   @moduledoc """
   Documentation for `SwiftElixirTest`.
   """
+
+  @on_load :load_nif
+
+  def load_nif do
+    nif_file = '#{:code.priv_dir(:swift_elixir_test)}/.libs/libnif'
+
+    case :erlang.load_nif(nif_file, 0) do
+      :ok -> :ok
+      {:error, {:reload, _}} -> :ok
+      {:error, reason} -> Logger.warn("Failed to load NIF: #{inspect(reason)}")
+    end
+  end
+
+  def test(), do: raise("NIF test/0 not implemented")
 end
