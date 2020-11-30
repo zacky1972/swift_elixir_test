5504bb74289169e6a3a0b43d50bd15ba3a0185b5
Susumu Yamazaki <zacky1972@gmail.com>
2020-12-01 05:29:21 +0900

    detect Foundation and NSLog in caller.m

--- native/caller.m
+++ native/caller.m
@@ -1,7 +1,12 @@
+#ifdef EXIST_FOUNDATION
 #import <Foundation/Foundation.h>
+#endif
+
 #import "caller.h"
 
 void caller()
 {
+#ifdef WORK_NSLOG
 	NSLog(@"Hello world from Objective-C.");
+#endif
 }
