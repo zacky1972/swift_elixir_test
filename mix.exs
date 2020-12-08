2edb1f6e4356af16cf42d8aa97f7904152826daa
Susumu Yamazaki <zacky1972@gmail.com>
2020-12-08 22:42:39 +0900

    implement get_arch

--- mix.exs
+++ mix.exs
@@ -36,6 +36,66 @@ defmodule SwiftElixirTest.MixProject do
   end
 
   defp configure(_args) do
-    System.cmd("#{File.cwd!()}/configure", [])
+    arch = get_arch(System.get_env("REBAR_TARGET_ARCH"))
+
+    System.cmd(
+      "#{File.cwd!()}/configure",
+      ["--prefix=#{Mix.Project.app_path()}/priv", "--host=#{arch}", "--target=#{arch}"]
+    )
   end
+
+  defp get_arch(nil) do
+    lib_dir =
+      System.get_env(
+        "ERL_EI_LIBDIR",
+        :code.root_dir() |> to_string() |> Kernel.<>("/usr/lib")
+      )
+
+    System.cmd("ar", ["-xv", "#{lib_dir}/libei.a", "ei_compat.o"])
+
+    r =
+      case System.cmd("file", ["ei_compat.o"]) do
+        {result, 0} ->
+          l = String.split(result)
+
+          arch =
+            Enum.filter(
+              %{
+                "x86_64" => l |> Enum.filter(&String.match?(&1, ~r/x86.64/)) |> length,
+                "arm64" => l |> Enum.filter(&String.match?(&1, ~r/arm64/)) |> length,
+                "aarch64" => l |> Enum.filter(&String.match?(&1, ~r/aarch64/)) |> length
+              },
+              fn {_, v} -> v != 0 end
+            )
+
+          platform =
+            Enum.filter(
+              %{
+                "apple-darwin" => l |> Enum.filter(&String.match?(&1, ~r/Mach-O/)) |> length,
+                "linux-gnu" => l |> Enum.filter(&String.match?(&1, ~r/ELF/)) |> length
+              },
+              fn {_, v} -> v != 0 end
+            )
+
+          "#{arch |> hd() |> elem(0)}-#{platform |> hd() |> elem(0)}"
+
+        _ ->
+          {arch, 0} = System.cmd("uname", ["-m"])
+          {s, 0} = System.cmd("uname", ["-s"])
+
+          platform =
+            case s do
+              "Linux" -> "linux-gnu"
+              "Darwin" -> "apple-darwin"
+              _ -> raise "unsupported platform"
+            end
+
+          "#{arch}-#{platform}"
+      end
+
+    File.rm("ei_compat.o")
+    r
+  end
+
+  defp get_arch(arch), do: arch
 end
